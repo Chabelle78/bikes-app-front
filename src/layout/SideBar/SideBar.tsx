a944389f0b35d9f719f7532f9deb8d285696ce89
Chabelle78 <78235274+Chabelle78@users.noreply.github.com>
2026-02-18 20:36:26 +0100

    [VERSION] implement versionning V1 (#23)

--- src/layout/SideBar/SideBar.tsx
+++ src/layout/SideBar/SideBar.tsx
@@ -4,35 +4,38 @@ import { useAppDispatch, useAppSelector } from "@/app/hooks";
 import { clearFilters } from "@/features/bikesSlice";
 import { selectAreFiltersDisabled } from "@/features/settingsSlice";
 
-import styles from './SideBar.module.scss';
+import styles from "./SideBar.module.scss";
 
 export default function SidesBar() {
-    const dispatch = useAppDispatch();
-    const areFiltersDisabled = useAppSelector(selectAreFiltersDisabled);
+  const dispatch = useAppDispatch();
+  const areFiltersDisabled = useAppSelector(selectAreFiltersDisabled);
 
-    const handleReset = () => {
-        if (!areFiltersDisabled) {
-            dispatch(clearFilters());
-        }
-    };
+  const handleReset = () => {
+    if (!areFiltersDisabled) {
+      dispatch(clearFilters());
+    }
+  };
 
-    return (
-        <div className={styles.filtersContainer}>
-            <div className={styles.header}>
-                <h2 className={styles.title}>Filtres</h2>
-                <button 
-                    className={styles.resetButton} 
-                    onClick={handleReset}
-                    disabled={areFiltersDisabled}
-                >
-                    Réinitialiser
-                </button>
-            </div>
-            
-            <div className={styles.filtersContent}>
-                <SearchBar disabled={areFiltersDisabled} />   
-                <Filters disabled={areFiltersDisabled} />
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.filtersContainer}>
+      <div className={styles.header}>
+        <h2 className={styles.title}>Filtres</h2>
+        <button
+          className={styles.resetButton}
+          onClick={handleReset}
+          disabled={areFiltersDisabled}
+        >
+          Réinitialiser
+        </button>
+      </div>
+
+      <div className={styles.filtersContent}>
+        <SearchBar disabled={areFiltersDisabled} />
+        <Filters disabled={areFiltersDisabled} />
+      </div>
+      <div className={styles.versionning}>
+        <p>V1.0.0</p>
+      </div>
+    </div>
+  );
 }
